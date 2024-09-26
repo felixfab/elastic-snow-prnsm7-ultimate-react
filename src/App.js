a4239e1d4fbd59c12528747f12fa5eb9f32b6702
felixfab <65343441+felixfab@users.noreply.github.com>
2024-09-26 05:43:30 +0000

    feat: add advice id

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
+  const [id, setId] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setId(data.slip.id);
     setCount((c) => c + 1);
   }
 
@@ -18,6 +20,7 @@ export default function App() {
   return (
     <div>
       <h1>{advice}</h1>
+      <h2>{id}</h2>
       <button onClick={getAdvice}>Get advice</button>
       <Message count={count} />
     </div>
